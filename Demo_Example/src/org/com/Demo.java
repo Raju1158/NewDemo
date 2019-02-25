9ca5dfa6f7fd757597faa42024dbae8136b2450a
admin <admin@DESKTOP-O456U7C>
2019-02-25 13:16:41 +0530

    added method

--- Demo_Example/src/org/com/Demo.java
+++ Demo_Example/src/org/com/Demo.java
@@ -2,9 +2,19 @@ package org.com;
 
 public class Demo {
 
+	
+	static int a=40;
+	static int b=30;
+	public int add(int a,int b)
+	{
+		return a+b;
+		
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      Demo demo=new Demo();
+      demo.add(a, b);
 	}
 
 }
