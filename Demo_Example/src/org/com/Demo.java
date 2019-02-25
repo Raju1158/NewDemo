4b2949a09fa09d536d94bca59d2c51611d7f3981
admin <admin@DESKTOP-O456U7C>
2019-02-25 13:56:53 +0530

    Changed

--- Demo_Example/src/org/com/Demo.java
+++ Demo_Example/src/org/com/Demo.java
@@ -2,14 +2,12 @@ package org.com;
 
 public class Demo {
 
-	
 	static int a=40;
 	static int b=30;
 	public int add(int a,int b)
 	{
 		return a+b;
-		
-		
+
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
